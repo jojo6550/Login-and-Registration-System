8105619890b4eefd5fdb737c2f3cb2ac23ab89e0
jojo6550 <josiah.johnson6550@gmail.com>
2024-12-30 22:02:09 -0500

    `Added password encryption and verification, user database check, and updated file associations in VSCode settings.`

--- main.cpp
+++ main.cpp
@@ -1,31 +1,59 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 string encrypt(string password);
 string login(string info[2]);
 void draw();
 void reg();
 int main(){
-    draw();
     reg();
-    draw();
+
     return 0;
 }
 
+string ecrypt(string password){
+    string newPassword = "";
+    for(char c: password){
+        newPassword += c + 2;
+    }
+    cout << newPassword <<endl;
+    return newPassword;
+}
+
 void reg(){
-    string username, password;
+    string username, password, verifyPassword;
     cout << "Enter username: ";
     getline(cin, username);
     cout << "Enter password using 8+ characters (e.g. p@SsW0$d): ";
     cin >> password;
+    cout << "Enter password to verify: ";
+    cin >> verifyPassword;
      
     while(password.size() < 8){
-        cout << "Password must be 8+ characters long."<<endl;
+        cout<<"Password was too short."<<endl;
         cout << "Enter password using 8+ characters (e.g. p@SsW0$d): ";
         cin >> password;
+        cout << "Enter password to verify: ";
+        cin >> verifyPassword;
     }
-}
+    while(password != verifyPassword){
+        cout<<"Passwords do not match."<<endl;
+        cout << "Enter password using 8+ characters (e.g. p@SsW0$d): ";
+        cin >> password;
+        cout << "Enter password to verify: ";
+        cin >> verifyPassword;
+    }
 
-void draw(){
-    cout<<"****************************************\n";
+    cout << ecrypt(password);
+    ifstream infile("database.txt");
+    string line;
+    while(getline(infile, line)){
+        if(line.find(username) + ":" == 0){
+            cout<<"User exists. Try again."<<endl;
+            infile.close();
+            return;
+        }
+    }
+    infile.close();
 }
